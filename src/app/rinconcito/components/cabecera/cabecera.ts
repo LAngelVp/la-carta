7dbe29f043146989456c634fd7e4e216974d6d24
Luis Angel <neal.ocaffry@gmail.com>
2025-09-29 16:43:15 -0600

    avance

--- src/app/rinconcito/components/cabecera/cabecera.ts
+++ src/app/rinconcito/components/cabecera/cabecera.ts
@@ -1,5 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import 'aos/dist/aos.css';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-cabecera',
@@ -9,8 +11,17 @@ import { Component } from '@angular/core';
   templateUrl: './cabecera.html',
   styleUrl: './cabecera.css'
 })
-export class Cabecera {
+export class Cabecera implements OnInit {
   imgCabecera : string = "/elRinconcito/logosIconos/oceano.jpg"
   logotipo : string = "/elRinconcito/logosIconos/logotipoColor.png"
   urlFacebook : string ="https://www.facebook.com/ElRinconcitoYanga"
+
+  ngOnInit() {
+    AOS.init({
+      duration: 1000, // duración de la animación en ms
+      easing: 'ease-in-out', // tipo de easing
+      once: true, // si la animación solo ocurre una vez
+      offset: 100 // offset (en px) desde el elemento original
+    });
+  }
 }
