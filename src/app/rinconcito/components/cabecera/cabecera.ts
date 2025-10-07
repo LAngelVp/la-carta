f5106392f1962e3a6bc3e9c253a6fea21d7dba5b
Luis Angel <neal.ocaffry@gmail.com>
2025-10-07 17:02:31 -0600

    avances

--- src/app/rinconcito/components/cabecera/cabecera.ts
+++ src/app/rinconcito/components/cabecera/cabecera.ts
@@ -10,8 +10,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './cabecera.css'
 })
 export class Cabecera {
-  imgCabecera : string = "/elRinconcito/logosIconos/portada.jpg"
-  logotipo : string = "/elRinconcito/logosIconos/logotipoColor.png"
-  urlFacebook : string ="https://www.facebook.com/ElRinconcitoYanga"
+  imgCabecera: string = "/elRinconcito/logosIconos/portada.jpg";
+  logotipo: string = "/elRinconcito/logosIconos/logotipoColor.png";
+  
+  botonesContacto: any = [
+    {
+      name: "Facebook",
+      url: "https://www.facebook.com/ElRinconcitoYanga",
+      image: "/icons/facebookWhite.svg",
+      accion: () => this.redirigir(this.botonesContacto[0].url)
+    },
+    {
+      name: "WhatApp",
+      url: "https://wa.me/522787387082",
+      image: "/icons/whatsappWhite.svg",
+      accion: () => this.redirigir(this.botonesContacto[1].url)
+    },
+    {
+      name: "Ubicación",
+      url: "https://maps.app.goo.gl/zWmeTnLzBzuLgB1t9",
+      image: "/icons/UbicacionMundo.svg",
+      accion: () => this.redirigir(this.botonesContacto[2].url)
+    },
+  ];
 
+  // Función única que reutiliza las URLs del JSON
+  redirigir(url: string) {
+    window.open(url, '_blank');
+  }
+
+  // Función general para manejar clics
+  manejarClic(boton: any) {
+    if (boton.accion) {
+      boton.accion();
+    } else if (boton.url) {
+      this.redirigir(boton.url);
+    }
+  }
 }
